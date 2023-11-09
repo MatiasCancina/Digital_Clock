8196caee2edf83d65ce2e58fcf0466126a92929a
Matias Cancina <cancinamatias@gmail.com>
2023-11-09 16:02:30 -0300

    responsive

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -23,23 +23,23 @@ const Clock: React.FC = () => {
   const today: string = daysOfWeek[time.getDay()];
 
   return (
-    <div className="flex flex-col justify-center bg-blue1000 space-y-5 px-16 py-12 rounded-full text-blue100 text-6xl shadow-2xl shadow-blue800">
+    <div className="flex flex-col justify-center bg-blue1000 space-y-4 lg:space-y-5 px-12 lg:px-16 py-10 lg:py-12 rounded-full text-blue100 shadow-2xl shadow-blue800">
       <div>
-        <p className="text-2xl font-medium space-x-5">
+        <p className="text-base lg:text-2xl font-medium lg:space-x-5">
           {daysOfWeek.map((day, index) => (
             <span key={index} className={isToday(new Date()) && today === day ? 'text-blue300' : 'text-blue800'}>
               {day}{' '}
             </span>
           ))}
         </p>
-        <span className="font-clock font-mono font-bold text-9xl">
+        <span className="font-clock font-mono font-bold text-5xl lg:text-9xl">
           {hours}:{minutes}:{seconds}
         </span>
-        <p className='text-2xl'>
+        <p className='text-xl lg:text-2xl'>
           {formattedDate}
         </p>
       </div>
-      <h6 className="text-base font-normal underline underline-offset-4">DIGITAL CLOCK with Typescript</h6>
+      <h6 className="text-xs lg:text-base font-normal underline underline-offset-4">DIGITAL CLOCK with Typescript</h6>
     </div>
   );
 };
